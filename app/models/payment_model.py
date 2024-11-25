9d40d361884a01c62857047e205a2d61219da1d2
Imanmaris <imanmaris.work@gmail.com>
2024-11-25 18:37:10 +0700

    refactor:Add new column in payments models

--- app/models/payment_model.py
+++ app/models/payment_model.py
@@ -14,16 +14,10 @@ class PaymentModel(Base):
     id = Column(CHAR(36), primary_key=True, default=lambda: str(uuid.uuid4()), unique=True, index=True)
     order_id = Column(CHAR(36), ForeignKey("orders.id"), nullable=False, unique=True, index=True)  # UUID from OrderModel
     transaction_id = Column(String(255), unique=True, nullable=False)  # ID transaksi dari Midtrans
-    payment_type = Column(String(50), nullable=False)  # e.g., credit_card, bank_transfer
+    payment_type = Column(String(50), nullable=True)  # e.g., credit_card, bank_transfer
     gross_amount = Column(DECIMAL(10, 2), nullable=False)
-    transaction_status = Column(
-        Enum(TransactionStatusEnum),  # Menggunakan Enum untuk status transaksi
-        nullable=False
-    )
-    fraud_status = Column(
-        Enum(FraudStatusEnum),  # Menggunakan Enum untuk status fraud transaksi
-        nullable=True
-    )
+    transaction_status = Column(Enum(TransactionStatusEnum), nullable=True)
+    fraud_status = Column(Enum(FraudStatusEnum), nullable=True)
     payment_response = Column(JSON, nullable=True)  # menyimpan respon lengkap dari Midtrans
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
